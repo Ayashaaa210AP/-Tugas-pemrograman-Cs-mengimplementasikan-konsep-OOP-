47cda6f98aecf5455191084701b09d85975f4de6
Ayashaaa210AP <arghaarybawa@gmail.com>
2025-09-18 21:30:16 +0700

    test

---
+++ ProgramRentalMobil/ProgramRentalMobil/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Mobil
+{
+    public string plat;
+    public string merk;
+    public bool tersedia;
+
+    public Mobil(string p, string m)
+    {
+        plat = p;
+        merk = m;
+        tersedia = true;
+    }
+
+    public virtual int HitungBiaya(int hari)
+    {
+        return hari * 500000;
+    }
+
+    public void Sewa() => tersedia = false;
+    public void Kembali() => tersedia = true;
+}
